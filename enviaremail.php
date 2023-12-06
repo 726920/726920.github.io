abe03d77a7f7da2f64bd036f2eac0c3b59e4a892
726920 <34869143+726920@users.noreply.github.com>
2023-12-06 19:11:38 -0300

    Add files via upload

--- enviaremail.php
+++ enviaremail.php
@@ -1,54 +1,51 @@
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
          Criando nossas variáveis para guardar as informações do formulário
-        $nomesobrenome=$_POST['nomesobrenome'];
+        $name=$_POST['name'];
         $Whatsapp=$_POST['Whatsapp'];
         $email=$_POST['email'];
         //$radio=$_POST['novidades'];
         //$date=date("d/m/Y");
-        $msg=$_POST['mensagem'];
+        $message=$_POST['mensagem'];
         
-        //echo "<p>Olá seu nome :, ". $nomesobrenome ."</p>"; 
-        //echo "<p>Seu telefone : ". $Whatsapp ."</p>";
-        //echo "<p>Seu email : ". $email ."</p>";  
-    }
+            }
 
    
     
     // formatando nossa mensagem (que será envaida ao e-mail)
     $mensagem= 'Esta mensagem foi enviada através do formulário<br><br>';
-    $mensagem.='<b>Nome: </b>'.$nomesobrenome.'<br>';
+    $mensagem.='<b>Nome: </b>'.$name.'<br>';
     $mensagem.='<b>Telefone:</b> '.$Whatsapp.'<br>';
     $mensagem.='<b>E-Mail:</b> '.$email.'<br>';
     //$mensagem.='<b>Deseja receber novidades:</b> '.$radio.'<br>';
     //$mensagem.='<b>Data de envio:</b> '.$date.'<br>';
-    $mensagem.='<b>Mensagem:</b> '.$msg.'<br>';
+    $mensagem.='<b>Mensagem:</b> '.$message.'<br>';
 
        
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+    use vendor\phpMailer\phpMailer\src\PHPMailer;
+    use vendor\phpMailer\phpMailer\src\Exception;
  
     require 'vendor/autoload.php';
  
    $mail = new PHPMailer(true);
  
 try {
-    $mail->SMTPDebug = 2;            
+    $mail->SMTPDebug = 1;            
 
     $mail->isSMTP(); // Não modifique
-    $mail->Host       = 'smtp.gmail.com';  // SEU HOST (HOSPEDAGEM)
+    $mail->Host       = 'mail.reunatech.vip';  // SEU HOST (HOSPEDAGEM)
     $mail->SMTPAuth   = true;                        // Manter em true
-    $mail->Username   = 'acelsopinheiro@zcttech.com';   //SEU USUÁRIO DE EMAIL
-    $mail->Password   = '456920Ao';                   //SUA SENHA DO EMAIL SMTP password
-    $mail->SMTPSecure = 'ssl';    //TLS OU SSL-VERIFICAR COM A HOSPEDAGEM
-    $mail->Port       = 587;     //TCP PORT, VERIFICAR COM A HOSPEDAGEM
+    $mail->Username   = 'admreuna@reunatech.vip';   //SEU USUÁRIO DE EMAIL
+    $mail->Password   = '9302';                   //SUA SENHA DO EMAIL SMTP password
+    $mail->SMTPSecure = 'tls';    //TLS OU SSL-VERIFICAR COM A HOSPEDAGEM
+    $mail->Port       = '465';     //TCP PORT, VERIFICAR COM A HOSPEDAGEM
     $mail->CharSet = 'UTF-8';    //DEFINE O CHARSET UTILIZADO
     
-    //Recipients
-    $mail->setFrom('acelsopinheiro@zcttech.com','ReunaTech');  //DEVE SER O MESMO EMAIL DO USERNAME
-    $mail->addAddress('adm@reunatech.vip');     // QUAL EMAIL RECEBERÁ A MENSAGEM!
+    //Recipient
+    $mail->setFrom('admreuna@reunatech.vip','ReunaTech');  //DEVE SER O MESMO EMAIL DO USERNAME
+    $mail->addAddress('acapinheirodrive@gmail.com');     // QUAL EMAIL RECEBERÁ A MENSAGEM!
     // $mail->addAddress('ellen@example.com');    // VOCÊ pode incluir quantos receptores quiser
-    $mail->addReplyTo($email, $nome);  //AQUI SERA O EMAIL PARA O QUAL SERA RESPONDIDO                  
+    $mail->addReplyTo($email, $name);  //AQUI SERA O EMAIL PARA O QUAL SERA RESPONDIDO                  
     // $mail->addCC('cc@example.com'); //ADICIONANDO CC
     // $mail->addBCC('bcc@example.com'); //ADICIONANDO BCC
 
@@ -59,7 +56,7 @@ try {
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Mensagem do Formulário'; //ASSUNTO
-    $mail->Body    = $mensagem;  //CORPO DA MENSAGEM
+    $mail->Body    = $mensagem;  //CORPO DA MENSSSAGEM
     $mail->AltBody = $mensagem;  //CORPO DA MENSAGEM EM FORMA ALT
 
 // Limpa os destinatários e os anexos
